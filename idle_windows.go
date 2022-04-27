ae7e64c8cdbd809da7c9baf08a022143ff98bcae
PaulDotSH <paulandreiabrudan@pm.me>
2022-04-27 18:32:58 +0300

    windows support

--- idle_windows.go
+++ idle_windows.go
@@ -3,37 +3,40 @@
 
 package go_idle_info
 
-func AwaitIdleTime(duration time.Duration) {
-
-}
-
-//to change
-/*
-var (
-    user32            = syscall.MustLoadDLL("user32.dll")
-    kernel32          = syscall.MustLoadDLL("kernel32.dll")
-    getLastInputInfo  = user32.MustFindProc("GetLastInputInfo")
-    getTickCount      = kernel32.MustFindProc("GetTickCount")
-    lastInputInfo struct {
-        cbSize uint32
-        dwTime uint32
-    }
+import (
+	"syscall"
+	"time"
+	"unsafe"
 )
 
-func IdleTime() time.Duration {
-    lastInputInfo.cbSize = uint32(unsafe.Sizeof(lastInputInfo))
-    currentTickCount, _, _ := getTickCount.Call()
-    r1, _, err := getLastInputInfo.Call(uintptr(unsafe.Pointer(&lastInputInfo)))
-    if r1 == 0 {
-            panic("error getting last input info: " + err.Error())
-    }
-    return time.Duration((uint32(currentTickCount) - lastInputInfo.dwTime)) * time.Millisecond
+var (
+	user32           = syscall.MustLoadDLL("user32.dll")
+	kernel32         = syscall.MustLoadDLL("kernel32.dll")
+	getLastInputInfo = user32.MustFindProc("GetLastInputInfo")
+	getTickCount     = kernel32.MustFindProc("GetTickCount")
+	lastInputInfo    struct {
+		cbSize uint32
+		dwTime uint32
+	}
+)
+
+func AwaitIdleTime(duration time.Duration) {
+	t := time.NewTicker(refresh * time.Millisecond)
+	for range t.C {
+		idle := IdleTime()
+		//fmt.Println(idle)
+		if idle > duration {
+			break
+		}
+	}
 }
 
-func main() {
-    t := time.NewTicker(1 * time.Second)
-    for range t.C {
-        fmt.Println(IdleTime())
-    }
+func IdleTime() time.Duration {
+	lastInputInfo.cbSize = uint32(unsafe.Sizeof(lastInputInfo))
+	currentTickCount, _, _ := getTickCount.Call()
+	r1, _, err := getLastInputInfo.Call(uintptr(unsafe.Pointer(&lastInputInfo)))
+	if r1 == 0 {
+		panic("error getting last input info: " + err.Error())
+	}
+	return time.Duration((uint32(currentTickCount) - lastInputInfo.dwTime)) * time.Millisecond
 }
-*/
